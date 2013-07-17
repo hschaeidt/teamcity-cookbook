f3f7aff1f79f1f2b06da5ed967483cc0b7eeb2e7
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2013-07-18 01:18:16 +0400

    Update teamcity version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["teamcity_server"]["version"]               = "7.1.4"
+default["teamcity_server"]["version"]               = "8.0.1"
 default["teamcity_server"]["build_agent"]["server"] = nil
 default["teamcity_server"]["build_agent"]["name"]   = nil
 default["teamcity_server"]["server"]["address"]     = "0.0.0.0"
