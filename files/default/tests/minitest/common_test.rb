b7125cd26a7310016e54317af112581ae27d8b27
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2013-07-19 14:49:43 +0400

    Use resources to assert

--- files/default/tests/minitest/common_test.rb
+++ files/default/tests/minitest/common_test.rb
@@ -4,11 +4,10 @@ describe_recipe "teamcity_server::common" do
   include Helpers::TeamcityServer
 
   it "downloads teamcity archive" do
-    assert ::File.exists? "/opt/TeamCity-#{node["teamcity_server"]["version"]}.tar.gz"
+    file("/opt/TeamCity-#{node["teamcity_server"]["version"]}.tar.gz").must_exist
   end
 
   it "unarchives teamcity" do
-    assert ::File.exists? "/opt/TeamCity"
-    assert ::File.directory? "/opt/TeamCity"
+    directory("/opt/TeamCity").must_exist
   end
 end
