d20a4441b36444d4c9fa15f8a2a3ba1e6f4fadb2
Lloyd Philbrook <lloyd@tablexi.com>
2014-03-26 20:06:17 +0800

    move user and group resource

--- recipes/common.rb
+++ recipes/common.rb
@@ -4,9 +4,20 @@ archive_name = "TeamCity-#{node["teamcity_server"]["version"]}.tar.gz"
 full_url     = "#{node["teamcity_server"]["base_url"]}#{archive_name}"
 archive      = "#{Chef::Config[:file_cache_path]}/#{archive_name}"
 
+group "teamcity" do
+  action :create
+end
+
+user "teamcity" do
+  action :create
+  gid "teamcity"
+  home node["teamcity_server"]["root_dir"]
+  shell "/bin/bash"
+end
+
 directory node["teamcity_server"]["root_dir"] do
-  owner  node["teamcity_server"]["user"]   
-  group  node["teamcity_server"]["group"] 
+  owner  node["teamcity_server"]["user"]
+  group  node["teamcity_server"]["group"]
   mode "0755"
   action :create
 end
@@ -14,27 +25,16 @@ end
 remote_file archive do
   backup false
   source full_url
-  owner  node["teamcity_server"]["user"]   
-  group  node["teamcity_server"]["group"] 
+  owner  node["teamcity_server"]["user"]
+  group  node["teamcity_server"]["group"]
   action :create_if_missing
   notifies :run, "execute[unarchive]", :immediately
 end
 
 execute "unarchive" do
   command "tar xf #{archive} --strip=1"
-  user   node["teamcity_server"]["user"]   
-  group  node["teamcity_server"]["group"] 
+  user   node["teamcity_server"]["user"]
+  group  node["teamcity_server"]["group"]
   cwd node["teamcity_server"]["root_dir"]
   action :nothing
-end
-
-group "teamcity" do
-	action :create
-end
-
-user "teamcity" do
-	action :create
-	gid "teamcity"
-	home node["teamcity_server"]["root_dir"]  
-	shell "/bin/bash"
 end
