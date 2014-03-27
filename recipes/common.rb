c74185c63846756b09dd1c1e2daf86114ee24a45
willejs <williamejsalt@gmail.com>
2014-03-27 11:38:15 +0000

    reordered user creation to appropriate place

--- recipes/common.rb
+++ recipes/common.rb
@@ -1,8 +1,15 @@
 include_recipe "java"
 
-archive_name = "TeamCity-#{node["teamcity_server"]["version"]}.tar.gz"
-full_url     = "#{node["teamcity_server"]["base_url"]}#{archive_name}"
-archive      = "#{Chef::Config[:file_cache_path]}/#{archive_name}"
+group "teamcity" do
+	action :create
+end
+
+user "teamcity" do
+	action :create
+	gid "teamcity"
+	home node["teamcity_server"]["root_dir"]  
+	shell "/bin/bash"
+end
 
 directory node["teamcity_server"]["root_dir"] do
   owner  node["teamcity_server"]["user"]   
@@ -10,6 +17,10 @@ directory node["teamcity_server"]["root_dir"] do
   mode "0755"
   action :create
 end
+
+archive_name = "TeamCity-#{node["teamcity_server"]["version"]}.tar.gz"
+full_url     = "#{node["teamcity_server"]["base_url"]}#{archive_name}"
+archive      = "#{Chef::Config[:file_cache_path]}/#{archive_name}"
 
 remote_file archive do
   backup false
@@ -26,15 +37,4 @@ execute "unarchive" do
   group  node["teamcity_server"]["group"] 
   cwd node["teamcity_server"]["root_dir"]
   action :nothing
-end
-
-group "teamcity" do
-	action :create
-end
-
-user "teamcity" do
-	action :create
-	gid "teamcity"
-	home node["teamcity_server"]["root_dir"]  
-	shell "/bin/bash"
 end
