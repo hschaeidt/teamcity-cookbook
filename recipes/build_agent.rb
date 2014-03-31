03de3c117f8208ba480a16489685a82150cbdb1a
Lloyd Philbrook <lloyd@tablexi.com>
2014-03-31 17:25:55 +0800

    allow multiple build agents per server

--- recipes/build_agent.rb
+++ recipes/build_agent.rb
@@ -14,44 +14,56 @@ if node["teamcity_server"]["build_agent"]["server"].nil?
   node.default["teamcity_server"]["build_agent"]["server"] = node["ipaddress"]
 end
 
-properties_file     = "#{node["teamcity_server"]["root_dir"]}/buildAgent/conf/buildAgent.properties"
-server              = node["teamcity_server"]["build_agent"]["server"]
-own_address         = node["ipaddress"]
-authorization_token = nil
+# Convert attributes to hash for easier parsing
+agent_defaults = JSON.parse(node["teamcity_server"]["build_agent"].to_json)
 
-if server == own_address
-  server = own_address = "127.0.0.1"
+agents = if node["teamcity_server"]["build_agents"].nil?
+  # If necessary, create a build_agents entry.
+  { "buildAgent" => agent_defaults }
+else
+  # Convert attributes to hash for easier parsing
+  JSON.parse(node["teamcity_server"]["build_agents"].to_json)
 end
 
-if File.exists?(properties_file)
-  lines = File.readlines(properties_file).grep(/^authorizationToken=/)
+agents.each do |agent, p|
+  properties          = agent_defaults.merge(p)
+  properties_file     = "#{node["teamcity_server"]["root_dir"]}/#{agent}/conf/buildAgent.properties"
+  server              = properties["server"]
+  own_address         = node["ipaddress"]
+  authorization_token = nil
 
-  unless lines.empty?
-    match = /authorizationToken=([0-9a-f]+)/.match(lines.first)
-    authorization_token = match[1] if match
+  if server == own_address
+    server = own_address = "127.0.0.1"
   end
 
-  unless node["teamcity_server"]["build_agent"]["name"]
-    lines = File.readlines(properties_file).grep(/^name=/)
+  if File.exists?(properties_file)
+    lines = File.readlines(properties_file).grep(/^authorizationToken=/)
 
     unless lines.empty?
-      match = /name=(.+)/.match(lines.first)
-      node.default["teamcity_server"]["build_agent"]["name"] = match[1].strip if match
+      match = /authorizationToken=([0-9a-f]+)/.match(lines.first)
+      authorization_token = match[1] if match
+    end
+
+    unless properties["name"]
+      lines = File.readlines(properties_file).grep(/^name=/)
+
+      unless lines.empty?
+        match = /name=(.+)/.match(lines.first)
+        properties["name"] = match[1].strip if match
+      end
     end
   end
+
+  template properties_file do
+    source "buildAgent.properties.erb"
+    owner  node["teamcity_server"]["user"]
+    group  node["teamcity_server"]["group"]
+    variables(
+      :server_address      => server,
+      :server_port         => node["teamcity_server"]["server"]["port"],
+      :name                => properties["name"],
+      :own_address         => own_address,
+      :authorization_token => authorization_token
+    )
+  end
 end
-
-template properties_file do
-  source "buildAgent.properties.erb"
-  owner  node["teamcity_server"]["user"]
-  group  node["teamcity_server"]["group"]
-  variables(
-    :server_address      => server,
-    :server_port         => node["teamcity_server"]["server"]["port"],
-    :name                => node["teamcity_server"]["build_agent"]["name"],
-    :own_address         => own_address,
-    :authorization_token => authorization_token
-  )
-end
-
-
