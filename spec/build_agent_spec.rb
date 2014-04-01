3085f1d6a12ab0728266840706b96bb5445c1d1d
Lloyd Philbrook <lloyd@tablexi.com>
2014-04-01 12:17:54 +0800

    add spec for multiple buildAgents

--- spec/build_agent_spec.rb
+++ spec/build_agent_spec.rb
@@ -2,7 +2,14 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 describe 'teamcity_server::build_agent' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set['teamcity_server']['build_agents'] = {
+        'buildAgent' => {},
+        'buildAgent1' => {}
+      }
+    end.converge(described_recipe)
+  end
 
   it 'include teamcity_server::common recipe' do
     expect(chef_run).to include_recipe('teamcity_server::common')
@@ -22,7 +29,11 @@ describe 'teamcity_server::build_agent' do
     end
   end
 
-  it 'create buildAgent.properties template' do
+  it 'create buildAgent.properties template for buildAgent' do
     expect(chef_run).to create_template("#{chef_run.node["teamcity_server"]["root_dir"]}/buildAgent/conf/buildAgent.properties")
+  end
+
+  it 'create buildAgent.properties template for buildAgent1' do
+    expect(chef_run).to create_template("#{chef_run.node["teamcity_server"]["root_dir"]}/buildAgent1/conf/buildAgent.properties")
   end
 end
